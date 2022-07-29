3e2bf9147a1ef7ab77c06d70373c58979f2ffe06
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-07-29 23:08:48 +0530

    Fix resources links

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,7 @@ export default function Home({ posts }) {
             </span>
           </h1>
           <div className="header-icons mb-2 flex">
-            <a
+            <Link
               aria-label="Send email"
               href="https://mail.google.com/mail/?view=cm&amp;fs=1&amp;to=arc@hyderabad.bits-pilani.ac.in"
               target="_blank"
@@ -62,8 +62,8 @@ export default function Home({ posts }) {
                   <path d="M464 64C490.5 64 512 85.49 512 112C512 127.1 504.9 141.3 492.8 150.4L275.2 313.6C263.8 322.1 248.2 322.1 236.8 313.6L19.2 150.4C7.113 141.3 0 127.1 0 112C0 85.49 21.49 64 48 64H464zM217.6 339.2C240.4 356.3 271.6 356.3 294.4 339.2L512 176V384C512 419.3 483.3 448 448 448H64C28.65 448 0 419.3 0 384V176L217.6 339.2z" />
                 </svg>
               </div>
-            </a>
-            <a
+            </Link>
+            <Link
               aria-label="My Github"
               target="_blank"
               href="https://github.com/arc-bphc"
@@ -79,8 +79,8 @@ export default function Home({ posts }) {
                   <path d="M186.1 328.7c0 20.9-10.9 55.1-36.7 55.1s-36.7-34.2-36.7-55.1 10.9-55.1 36.7-55.1 36.7 34.2 36.7 55.1zM480 278.2c0 31.9-3.2 65.7-17.5 95-37.9 76.6-142.1 74.8-216.7 74.8-75.8 0-186.2 2.7-225.6-74.8-14.6-29-20.2-63.1-20.2-95 0-41.9 13.9-81.5 41.5-113.6-5.2-15.8-7.7-32.4-7.7-48.8 0-21.5 4.9-32.3 14.6-51.8 45.3 0 74.3 9 108.8 36 29-6.9 58.8-10 88.7-10 27 0 54.2 2.9 80.4 9.2 34-26.7 63-35.2 107.8-35.2 9.8 19.5 14.6 30.3 14.6 51.8 0 16.4-2.6 32.7-7.7 48.2 27.5 32.4 39 72.3 39 114.2zm-64.3 50.5c0-43.9-26.7-82.6-73.5-82.6-18.9 0-37 3.4-56 6-14.9 2.3-29.8 3.2-45.1 3.2-15.2 0-30.1-.9-45.1-3.2-18.7-2.6-37-6-56-6-46.8 0-73.5 38.7-73.5 82.6 0 87.8 80.4 101.3 150.4 101.3h48.2c70.3 0 150.6-13.4 150.6-101.3zm-82.6-55.1c-25.8 0-36.7 34.2-36.7 55.1s10.9 55.1 36.7 55.1 36.7-34.2 36.7-55.1-10.9-55.1-36.7-55.1z" />
                 </svg>
               </div>
-            </a>
-            <a
+            </Link>
+            <Link
               aria-label="My Facebook"
               target="_blank"
               href="https://www.facebook.com/automationandroboticsclub"
@@ -96,27 +96,27 @@ export default function Home({ posts }) {
                   <path d="M400 32H48A48 48 0 0 0 0 80v352a48 48 0 0 0 48 48h137.25V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.27c-30.81 0-40.42 19.12-40.42 38.73V256h68.78l-11 71.69h-57.78V480H400a48 48 0 0 0 48-48V80a48 48 0 0 0-48-48z" />
                 </svg>
               </div>
-            </a>
+            </Link>
           </div>
           <div className="header-links">
-            <a className="link p-2 text-xl" href="#about" data-scroll="">
+            <Link className="link p-2 text-xl" href="#about" data-scroll="">
               About Us
-            </a>
-            <a className="link p-2 text-xl" href="#team" data-scroll="">
+            </Link>
+            <Link className="link p-2 text-xl" href="#team" data-scroll="">
               Our Team
-            </a>
-            <a className="link p-2 text-xl" href="#sponsors" data-scroll="">
+            </Link>
+            <Link className="link p-2 text-xl" href="#sponsors" data-scroll="">
               Sponsors
-            </a>
-            <a className="link p-2 text-xl" href="#contact" data-scroll="">
+            </Link>
+            <Link className="link p-2 text-xl" href="#contact" data-scroll="">
               Contact Us
-            </a>
-            <a className="header-vertical-line p-2 text-xl" data-scroll="">
+            </Link>
+            <Link className="header-vertical-line p-2 text-xl" data-scroll="">
               |
-            </a>
-            <a className="link p-2 text-xl" href="https://arc-bphc.github.io/site/resources/">
+            </Link>
+            <Link className="link p-2 text-xl" href="/resources/">
               Resources
-            </a>
+            </Link>
           </div>
         </div>
       </div>
