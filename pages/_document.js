42d8e0a90033d080112f35d496bfc50332efbd36
soumitradev <soumitra.shewale.dev@gmail.com>
2022-07-29 21:43:27 +0530

    Finish basics of landing page

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,12 @@ class MyDocument extends Document {
     return (
       <Html lang="en" className="scroll-smooth">
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap"
+            rel="stylesheet"
+          />
           <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
           <link
             rel="icon"
