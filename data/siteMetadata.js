7d26cec505d58cc62548f6f1f618763ef026b91b
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-08-07 00:10:38 +0530

    Fix Logo in header

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,9 +5,9 @@ const siteMetadata = {
   description: 'ARC Website made by tailwind.css and next.js',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: './arc-logo.svg',
+  siteUrl: 'https://arc-bphc.github.io',
+  siteRepo: 'https://github.com/arc-bphc/',
+  siteLogo: '/static/images/logo.png',
   image: 'https://arc-bphc.github.io/site/assets/images/logo/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   email: 'arc@hyderabad.bits-pilani.ac.in',
