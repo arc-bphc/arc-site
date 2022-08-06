49ccb9ab708e8acb3fb45eebe49617185c280037
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-08-06 23:58:40 +0530

    Fix favicons

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -7,7 +7,7 @@ const siteMetadata = {
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: '/static/images/arc-logo.png',
+  siteLogo: './arc-logo.svg',
   image: 'https://arc-bphc.github.io/site/assets/images/logo/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   email: 'arc@hyderabad.bits-pilani.ac.in',
