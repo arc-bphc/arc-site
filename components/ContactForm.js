adebd6b562984f6db1793e555f5b8114f2a6a9d9
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-08-07 22:58:40 +0530

    Fix form css and response

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -3,18 +3,43 @@ import { useForm, ValidationError } from '@formspree/react'
 function ContactForm() {
   const [state, handleSubmit] = useForm('mwkyvlqy')
   if (state.succeeded) {
-    return alert('Thanks for your message!')
+    return <div>Your message was sent. We will get back to you as soon as possible.</div>
   }
   return (
-    <form className='flex flex-col items-start' onSubmit={handleSubmit}>
-      <label htmlFor="name">Name</label>
-      <input className="text-black w-max" id="name" type="text" name="name" />
-      <label htmlFor="email">Email Address</label>
-      <input className="text-black" id="email" type="email" name="email" />
+    <form className="flex flex-col items-start" onSubmit={handleSubmit}>
+      <label htmlFor="name" className="text-white-700 block text-2xl font-medium">
+        Name
+      </label>
+      <input
+        className="my-5 block w-full rounded-md border-gray-300 pl-7 pr-12 text-gray-800 focus:border-indigo-500 focus:ring-indigo-500 sm:text-xl"
+        id="name"
+        type="text"
+        name="name"
+      />
+      <label htmlFor="email" className="text-white-700 block text-2xl font-medium">
+        Email Address
+      </label>
+      <input
+        className="my-5 block w-full rounded-md border-gray-300 pl-7 pr-12 text-gray-800 focus:border-indigo-500 focus:ring-indigo-500 sm:text-xl"
+        id="email"
+        type="email"
+        name="email"
+      />
       <ValidationError prefix="Email" field="email" errors={state.errors} />
-      <textarea className="text-black" id="message" name="message" />
+      <label htmlFor="email" className="text-white-700 block text-2xl font-medium">
+        Message
+      </label>
+      <textarea
+        className="my-5 block w-full rounded-md border-gray-300 pl-7 pr-12 text-gray-800 focus:border-indigo-500 focus:ring-indigo-500 sm:text-xl"
+        id="message"
+        name="message"
+      />
       <ValidationError prefix="Message" field="message" errors={state.errors} />
-      <button type="submit" disabled={state.submitting}>
+      <button
+        type="submit"
+        className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-xl font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        disabled={state.submitting}
+      >
         Submit
       </button>
     </form>
