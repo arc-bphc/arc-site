bcb5f41d578f881b4343858c3fa177828f510703
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-08-10 12:19:58 +0530

    Add Page numbers for every page

--- components/Pagination.js
+++ components/Pagination.js
@@ -3,31 +3,64 @@ import Link from '@/components/Link'
 export default function Pagination({ totalPages, currentPage }) {
   const prevPage = parseInt(currentPage) - 1 > 0
   const nextPage = parseInt(currentPage) + 1 <= parseInt(totalPages)
+  const pages = []
+
+  for (let i = 0; i < totalPages; i++) {
+    pages.push(i + 1)
+  }
 
   return (
     <div className="space-y-2 pt-6 pb-8 md:space-y-5">
       <nav className="flex justify-between">
         {!prevPage && (
-          <button rel="previous" className="cursor-auto disabled:opacity-50" disabled={!prevPage}>
+          <button
+            rel="previous"
+            className="cursor-auto p-2 disabled:opacity-50"
+            disabled={!prevPage}
+          >
             Previous
           </button>
         )}
+
         {prevPage && (
           <Link href={currentPage - 1 === 1 ? `/projects/` : `/projects/page/${currentPage - 1}`}>
-            <button rel="previous">Previous</button>
+            <button rel="previous" className="p-2">
+              Previous
+            </button>
           </Link>
         )}
-        <span>
-          {currentPage} of {totalPages}
-        </span>
+
+        {pages.map((pagenum) => {
+          if (pagenum == currentPage) {
+            return (
+              <span key={pagenum}>
+                <Link href={`/projects/page/${pagenum}`}>
+                  <button className="rounded-full border-2 border-solid p-2">{pagenum}</button>
+                </Link>
+              </span>
+            )
+          } else {
+            return (
+              <span key={pagenum}>
+                <Link href={`/projects/page/${pagenum}`}>
+                  <button className="p-2">{pagenum}</button>
+                </Link>
+              </span>
+            )
+          }
+        })}
+
         {!nextPage && (
-          <button rel="next" className="cursor-auto disabled:opacity-50" disabled={!nextPage}>
+          <button rel="next" className="cursor-auto p-2 disabled:opacity-50" disabled={!nextPage}>
             Next
           </button>
         )}
+
         {nextPage && (
           <Link href={`/projects/page/${currentPage + 1}`}>
-            <button rel="next">Next</button>
+            <button rel="next" className="p-2">
+              Next
+            </button>
           </Link>
         )}
       </nav>
