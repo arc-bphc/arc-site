5d4ef16276193f08d6cfc63deb0e959c1e198e26
Divyateja Pasupuleti <divyateja2004@gmail.com>
2022-08-10 12:22:22 +0530

    Fix BITS logo redirect to BPHC Website

--- pages/index.js
+++ pages/index.js
@@ -265,9 +265,12 @@ export default function Home({ posts }) {
         <footer className="w-full bg-white p-3 dark:bg-gray-900 sm:p-6">
           <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
-              <Link href="/" class="flex-column items-center md:flex">
+              <Link
+                href="https://www.bits-pilani.ac.in/hyderabad/"
+                className="flex-column items-center md:flex"
+              >
                 <img src="static/images/bits_logo.png" className="h-15 mx-2" alt="ARC Logo" />
-                {/* <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Automation and Robotics Club</span> */}
+                {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Automation and Robotics Club</span> */}
               </Link>
             </div>
             <div className="grid grid-cols-1 gap-8 sm:grid-cols-4 sm:gap-6">
@@ -293,7 +296,7 @@ export default function Home({ posts }) {
                   {resourcesData.map((resource) => {
                     return (
                       <li className="mb-1" key={resource.id}>
-                        <Link href={resource.href} class="hover:underline">
+                        <Link href={resource.href} className="hover:underline">
                           {resource.title}
                         </Link>
                       </li>
@@ -307,12 +310,12 @@ export default function Home({ posts }) {
                 </span>
                 <ul className="mt-4 text-gray-600 dark:text-gray-400">
                   <li className="mb-1">
-                    <Link href="/team" class="hover:underline ">
+                    <Link href="/team" className="hover:underline ">
                       Team
                     </Link>
                   </li>
                   <li>
-                    <Link href="/about" class="hover:underline">
+                    <Link href="/about" className="hover:underline">
                       About
                     </Link>
                   </li>
@@ -324,7 +327,7 @@ export default function Home({ posts }) {
           <div className="sm:flex sm:items-center sm:justify-between">
             <span className="text-sm text-gray-500 dark:text-gray-400 sm:text-center">
               © 2022{' '}
-              <Link href="/" class="hover:underline">
+              <Link href="/" className="hover:underline">
                 Automation and Robotics Club
               </Link>
               . All Rights Reserved.
@@ -332,7 +335,7 @@ export default function Home({ posts }) {
             <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
               <Link
                 href={siteMetadata.facebook}
-                class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+                className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
               >
                 <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                   <path
@@ -345,7 +348,7 @@ export default function Home({ posts }) {
               </Link>
               <Link
                 href={siteMetadata.instagram}
-                class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+                className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
               >
                 <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                   <path
@@ -358,7 +361,7 @@ export default function Home({ posts }) {
               </Link>
               <Link
                 href={siteMetadata.twitter}
-                class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+                className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
               >
                 <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                   <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
@@ -367,7 +370,7 @@ export default function Home({ posts }) {
               </Link>
               <Link
                 href={siteMetadata.github}
-                class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+                className="text-gray-500 hover:text-gray-900 dark:hover:text-white"
               >
                 <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                   <path
