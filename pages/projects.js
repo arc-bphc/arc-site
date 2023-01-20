8a4832e91cdfe87e687a0c46a877ec0cd395b0a4
Divyateja Pasupuleti <divyateja2004@gmail.com>
2023-01-20 16:57:37 +0530

    Fix projects page ui

--- pages/projects.js
+++ pages/projects.js
@@ -2,6 +2,8 @@ import { getAllFilesFrontMatter } from '@/lib/mdx'
 import siteMetadata from '@/data/siteMetadata'
 import ListLayout from '@/layouts/ListLayout'
 import { PageSEO } from '@/components/SEO'
+import Card from '@/components/Card'
+import { useState } from 'react'
 
 export const POSTS_PER_PAGE = 8
 
@@ -17,15 +19,66 @@ export async function getStaticProps() {
 }
 
 export default function Projects({ posts, initialDisplayPosts, pagination }) {
+  const [searchValue, setSearchValue] = useState('')
+
   return (
     <>
       <PageSEO title={`Projects - ${siteMetadata.author}`} description={siteMetadata.description} />
-      <ListLayout
+      {/* <ListLayout
         posts={posts}
         initialDisplayPosts={initialDisplayPosts}
         pagination={pagination}
         title="Projects"
-      />
+      /> */}
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <div className="space-y-2 px-4 pt-6 pb-8 md:space-y-5">
+            <h1 className="text-center text-4xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-left md:text-6xl md:leading-14">
+              Projects
+            </h1>
+          </div>
+
+          <div className="relative max-w-lg px-4">
+            <input
+              aria-label="Search articles"
+              type="text"
+              onChange={(e) => setSearchValue(e.target.value)}
+              placeholder="Search articles"
+              className="block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
+            />
+            <svg
+              className="absolute right-7 top-3 h-5 w-5 text-gray-400 dark:text-gray-300"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </div>
+
+          <div className="container py-4">
+            <div className="flex flex-wrap justify-center md:justify-start">
+              {posts
+                .filter((p) => p.title.toLowerCase().includes(searchValue.toLowerCase()))
+                .map((d) => (
+                  <Card
+                    key={d.title}
+                    title={d.title}
+                    description={d.description}
+                    imgSrc={d.cover}
+                    href={`/projects/` + d.slug}
+                  />
+                ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
